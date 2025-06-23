660b11071603434b314fe9e76aea95aebf89b0e8
hmfarid88 <hmfariduddin189@gmail.com>
2025-06-23 23:02:32 +0600

    updated

--- src/main/java/com/iyadsoft/billing_craft_backend/repository/ProductSaleRepository.java
+++ src/main/java/com/iyadsoft/billing_craft_backend/repository/ProductSaleRepository.java
@@ -77,10 +77,10 @@ public interface ProductSaleRepository extends JpaRepository<ProductSale, Long> 
         @Query("SELECT new com.iyadsoft.billing_craft_backend.dto.InvoiceDataDTO(c.cName, c.phoneNumber, c.address, c.soldby, ps.brand, ps.productName, ps.productno, ps.color, s.date, s.time, s.saleType, ps.pprice, s.sprice, s.discount, s.offer, c.cardPay, c.vatAmount, c.received, c.cid, s.saleId) FROM ProductSale s JOIN s.customer c JOIN s.productStock ps WHERE s.username=:username and c.cid=:cid")
         List<InvoiceDataDTO> getInvoiceDataByUsername(String username, String cid);
 
-        @Query("SELECT new com.iyadsoft.billing_craft_backend.dto.CashbookSaleDto(ps.date, ps.customer.cid, sum(ps.productStock.sprice + ps.customer.vatAmount-ps.discount-ps.offer-ps.customer.cardPay) as value) "
+        @Query("SELECT new com.iyadsoft.billing_craft_backend.dto.CashbookSaleDto(ps.date, ps.customer.cid, sum(ps.sprice + ps.customer.vatAmount-ps.discount-ps.offer-ps.customer.cardPay) as value) "
                         +
                         "FROM ProductSale ps " +
-                        "WHERE ps.saleType = 'customer' AND (ps.productStock.sprice-ps.discount-ps.offer)>0 AND ps.username=:username AND ps.date= :date group by ps.date, ps.customer.cid")
+                        "WHERE ps.saleType = 'customer' AND (ps.sprice-ps.discount-ps.offer)>0 AND ps.username=:username AND ps.date= :date group by ps.date, ps.customer.cid")
         List<CashbookSaleDto> findCustomerSalesDetails(@Param("username") String username, @Param("date") LocalDate date);
 
         @Query("SELECT SUM(COALESCE(ps.productStock.pprice, 0.0)) AS totalSoldValue " +
